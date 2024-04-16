47865212ed74cbe1650221ea01090809844f8bb7
Nancy Rojas <nrojas2@luc.edu>
2024-04-16 06:26:41 +0000

    deleted build

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /workspaces/NEEProject-NancyFork
+# Install script for directory: /workspaces/NEEProject
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -39,8 +39,8 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/workspaces/NEEProject-NancyFork/build/_deps/googletest-build/cmake_install.cmake")
-  include("/workspaces/NEEProject-NancyFork/build/rforth/cmake_install.cmake")
+  include("/workspaces/NEEProject/build/_deps/googletest-build/cmake_install.cmake")
+  include("/workspaces/NEEProject/build/rforth/cmake_install.cmake")
 
 endif()
 
@@ -52,5 +52,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/workspaces/NEEProject-NancyFork/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/workspaces/NEEProject/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- build/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ellamckenney/Documents/ssh:/NEEProject/googletest
-# Build directory: /Users/ellamckenney/Documents/ssh:/NEEProject/build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
