495bca7476b20677965c599a048bdfe8ec705f58
Nancy Rojas <nrojas2@luc.edu>
2024-04-16 11:58:03 -0500

    pulling from repo to make changes

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /workspaces/NEEProject-NancyFork
+# Install script for directory: /Users/nancyrojas/NEEProject
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -27,20 +27,21 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
-# Install shared libraries without execute permission?
-if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  set(CMAKE_INSTALL_SO_NO_EXE "1")
-endif()
-
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
+# Set default install directory permissions.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "/Library/Developer/CommandLineTools/usr/bin/objdump")
+endif()
+
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/workspaces/NEEProject-NancyFork/build/_deps/googletest-build/cmake_install.cmake")
-  include("/workspaces/NEEProject-NancyFork/build/rforth/cmake_install.cmake")
+  include("/Users/nancyrojas/NEEProject/build/_deps/googletest-build/cmake_install.cmake")
+  include("/Users/nancyrojas/NEEProject/build/Nancy-rforth_code/cmake_install.cmake")
+  include("/Users/nancyrojas/NEEProject/build/rforth/cmake_install.cmake")
 
 endif()
 
@@ -52,5 +53,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/workspaces/NEEProject-NancyFork/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/nancyrojas/NEEProject/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
