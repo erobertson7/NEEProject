e927002a312b57be56eb7a0298ed77dca65caaec
Ella McKenney <ellavmckenney@gmail.com>
2024-04-18 10:05:40 -0500

    Add int stack operator files.

---
+++ rforth/int_stack_operators.h
@@ -0,0 +1,50 @@
+// int_stack.h - interface to the int_stack
+
+#ifndef INT_STACK_H
+#define INT_STACK_H
+
+#include <stdio.h>
+#include <sys/queue.h>
+
+typedef struct int_entry {
+    int value;
+    SLIST_ENTRY(int_entry) entries;
+} int_entry_t;
+
+typedef struct int_stack {
+    SLIST_HEAD(stackhead, int_entry) head;
+    int size;
+    int capacity;
+} int_stack_t;
+
+extern void int_stack_init(int_stack_t *stk, int capacity);
+extern int int_stack_push(int_stack_t *stk, int value);
+extern int int_stack_pop(int_stack_t *stk, int *top_value);
+extern int int_stack_top(int_stack_t *stk, int *top_value);
+extern int int_stack_dup(int_stack_t *stk);
+extern int int_stack_swap(int_stack_t *stk);
+extern int int_stack_add(int_stack_t *stk);
+extern void int_stack_print(int_stack_t *stk, FILE *file);
+extern int int_stack_size(int_stack_t* stk);
+extern int int_stack_capacity(int_stack_t* stk);
+//added new tests here
+extern int int_stack_subtract(int_stack_t *stk);
+extern int int_stack_multiply(int_stack_t *stk);
+extern int int_stack_div(int_stack_t *stk);
+extern int int_stack_mod(int_stack_t *stk);
+extern int int_stack_dividemod(int_stack_t *stk);
+extern int int_stack_over(int_stack_t *stk);
+extern int int_stack_rot(int_stack_t *stk);
+extern int int_stack_drop(int_stack_t *stk);
+extern int int_stack_2swap(int_stack_t *stk);
+extern int int_stack_2dup(int_stack_t *stk);
+extern int int_stack_2drop(int_stack_t *stk);
+extern int int_stack_2over(int_stack_t *stk);
+//testing conditionals here (boolean expressions)
+extern int int_stack_equal(int_stack_t *stk);
+extern int int_stack_lessThan(int_stack_t *stk);
+extern int int_stack_greaterThan(int_stack_t *stk);
+extern int int_stack_and(int_stack_t *stk);
+extern int int_stack_or(int_stack_t *stk);
+extern int int_stack_invert(int_stack_t *stk);
+#endif // INT_STACK_H
