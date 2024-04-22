5bcdba0e57cd32fdfa11169c6d5f3bff784c4e53
Nancy Rojas <nrojas2@luc.edu>
2024-04-21 19:12:35 -0500

    Adding testing CMakeLists and Test file setup

--- rforth/int_stack_operations_Tests.cc
+++ rforth/int_stack_operations_Tests.cc
@@ -2,3 +2,17 @@
 #include <gtest/gtest.h>
 
 #include "int_stack_operators.hh"
+
+TEST(IntStackTests, Initialization)
+{
+    int_stack_t stack1;
+    int_stack_init(&stack1, 10);
+    ASSERT_EQ(int_stack_size(&stack1), 0);
+    ASSERT_EQ(int_stack_capacity(&stack1), 10);
+}
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
