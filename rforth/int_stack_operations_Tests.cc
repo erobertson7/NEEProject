63bd0cedad1b79d41f54a33dedc26559627a06f4
erobertson7 <104596760+erobertson7@users.noreply.github.com>
2024-04-21 22:08:27 -0500

    added test for max

--- rforth/int_stack_operations_Tests.cc
+++ rforth/int_stack_operations_Tests.cc
@@ -61,6 +61,23 @@ TEST(IntStackTests, int_stack_min){
     int_stack_print(&stack, stdout);
 }
 
+//Test for max
+TEST(IntStackTests, int_stack_max){
+    int_stack_t stack;
+    int max_value;
+    int_stack_init(&stack, 5);
+    int_stack_push(&stack, 1);
+    int_stack_push(&stack, -7);
+    int_stack_push(&stack, 3);
+    int_stack_push(&stack, 9);
+    int_stack_push(&stack, 4);
+    ASSERT_TRUE(int_stack_max(&stack));
+    int_stack_pop(&stack, &max_value);
+    ASSERT_EQ(max_value, 9);
+    int_stack_push(&stack, top_value);
+    int_stack_print(&stack, stdout);
+}    
+
 int main(int argc, char **argv)
 {
     testing::InitGoogleTest(&argc, argv);
