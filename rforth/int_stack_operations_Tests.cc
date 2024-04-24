d2765f8d8323f5c85d10ec64cef757a64771444c
Nancy Rojas <nrojas2@luc.edu>
2024-04-23 22:43:42 -0500

    debugged min and max tests and added abs test. All three are functioning Tests as of now! :)

--- rforth/int_stack_operations_Tests.cc
+++ rforth/int_stack_operations_Tests.cc
@@ -43,8 +43,24 @@ TEST(IntStackTests, int_stack_negate){
     int_stack_push(&stack, top_value);
     int_stack_print(&stack, stdout);
 }
+/* Used this to debug issues
+TEST(IntStackTests, int_stack_max){
+    int_stack_t stack;
+    int minimum_value;
+    int_stack_init(&stack, 5); //initialize stack
+    int_stack_push(&stack, 20);
+    int_stack_push(&stack, 600);
+    int_stack_push(&stack, 80);
+    int_stack_push(&stack, 10);
+    int_stack_print(&stack, stdout);//stack before
+    ASSERT_TRUE(int_stack_min(&stack));
+    int_stack_pop(&stack, &minimum_value);
+    ASSERT_EQ(minimum_value, 20);
+    int_stack_push(&stack, minimum_value);
+    int_stack_print(&stack, stdout); //stack after
+}
+*/
 
-/*
 
 //Test for min
 TEST(IntStackTests, int_stack_min){
@@ -55,11 +71,10 @@ TEST(IntStackTests, int_stack_min){
     int_stack_push(&stack, -4);
     int_stack_push(&stack, 11);
     int_stack_push(&stack, 6);
-    int_stack_push(&stack, 2);
     ASSERT_TRUE(int_stack_min(&stack));
     int_stack_pop(&stack, &min_value);
     ASSERT_EQ(min_value, -4);
-    int_stack_push(&stack, top_value);
+    int_stack_push(&stack, min_value);
     int_stack_print(&stack, stdout);
 }
 
@@ -67,19 +82,39 @@ TEST(IntStackTests, int_stack_min){
 TEST(IntStackTests, int_stack_max){
     int_stack_t stack;
     int max_value;
-    int_stack_init(&stack, 5);
+    int_stack_init(&stack, 6);
     int_stack_push(&stack, 1);
     int_stack_push(&stack, -7);
     int_stack_push(&stack, 3);
     int_stack_push(&stack, 9);
     int_stack_push(&stack, 4);
+    printf("Before max function\n");
+    int_stack_print(&stack, stdout);
     ASSERT_TRUE(int_stack_max(&stack));
     int_stack_pop(&stack, &max_value);
     ASSERT_EQ(max_value, 9);
-    int_stack_push(&stack, top_value);
+    int_stack_push(&stack, max_value);
+    printf("After max function\n");
     int_stack_print(&stack, stdout);
 }    
-*/
+
+TEST(IntStackTests, int_stack_abs){
+    int_stack_t stack;
+    int top_value;
+    int_stack_init(&stack, 3);
+    int_stack_push(&stack, 1);
+    int_stack_push(&stack, 3);
+    int_stack_push(&stack, -9);
+    printf("Before absolute function\n");
+    int_stack_print(&stack, stdout);
+    ASSERT_TRUE(int_stack_abs(&stack));
+    int_stack_pop(&stack, &top_value);
+    ASSERT_EQ(top_value, 9);
+    int_stack_push(&stack, top_value);
+    printf("After absolute function\n");
+    int_stack_print(&stack, stdout);
+}
+
 //Test for floor
 /*
 TEST(IntStackTests, int_stack_floor){
